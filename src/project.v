c4f0ca955cc73372759e11ff9a02f04068e60007
Alyan Salamat <sah240607@gmail.com>
2026-03-10 01:03:46 -0400

    added instance of peripheral to project.v

--- src/project.v
+++ src/project.v
@@ -17,9 +17,6 @@ module tt_um_uwasic_onboarding_alyan_salamat (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-
   assign uio_oe  = 8'hFF;
 
   // List all unused inputs to prevent warnings
@@ -28,6 +25,19 @@ module tt_um_uwasic_onboarding_alyan_salamat (
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
+
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .SCLK(ui_in[0]),
+    .COPI(ui_in[1]),
+    .nCS(ui_in[2]),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle)
+  );
 
   pwm_peripheral pwm_peripheral_isnt (
     .clk(clk),
@@ -40,6 +50,6 @@ module tt_um_uwasic_onboarding_alyan_salamat (
     .out({uio_out, uo_out})
   );
 
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
