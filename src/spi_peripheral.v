736ed1461500ed31a4ccf2c26059b1ebe5b40867
Alyan Salamat <sah240607@gmail.com>
2026-03-10 01:14:18 -0400

    names were incorrect

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -10,8 +10,8 @@ module spi_peripheral (
   // Outputs to Peripherals
   output reg [7:0] en_reg_out_7_0,
   output reg [7:0] en_reg_out_15_8,
-  output reg [7:0] en_reg_pwm_7_0,
-  output reg [7:0] en_reg_pwm_15_8,
+  output reg [7:0] en_pwm_out_7_0,
+  output reg [7:0] en_pwm_out_15_8,
   output reg [7:0] pwm_duty_cycle 
 );
 
